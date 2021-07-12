75632c07486914b4dab30c3d5dc9c29b617361e4
udberg <johnuduak066@gmail.com>
2021-07-12 23:20:38 +0100

    Implement booklist container

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookListContainer from './BookListContainer';
+import BooksFormContainer from './BooksFormContainer';
+
+const App = () => (
+    <div>
+        <BookListContainer />
+        <BookListContainer />
+    </div>
+);
+
+export default App;
