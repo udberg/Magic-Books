f514df73e1d58603688d48478595d3b528105f95
udberg <johnuduak066@gmail.com>
2021-07-13 00:10:05 +0100

    Prevent default DOM behaviour and create form categories

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -26,4 +26,32 @@ class BookForm extends Component {
         const { name, value } = event.target;
         this.setState({ [name]: value });
     }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const { createBook } = this.props'
+
+        if (this.isInValid) return;
+
+        if (createBook) {
+            createBook(this.state);
+        }
+
+        this.setState({
+          title: '',
+          category: '',  
+        });
+    }
+
+    render() {
+        const categories = [
+            'Action',
+            'Biography',
+            'History',
+            'Horror',
+            'Kids',
+            'Learning',
+            'Sci-Fi',
+        ];
+    }
 }
