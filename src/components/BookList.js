f30a9515beb22733757f45f90d8062072b7cf564
udberg <johnuduak066@gmail.com>
2021-07-13 15:14:32 +0100

    Render booklist component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,38 +3,38 @@ import PropTypes from 'prop-types';
 import BookContainer from '../containers/BookContainer';
 
 const BookList = ({ books, removeBook }) => (
-    <div>
-        <table>
-            <thead>
-                <tr>
-                    <th>Book ID</th>
-                    <th>Title</th>
-                    <th>Category</th>
-                </tr>
-            </thead>
-            <tbody>
-                {books.map((book) => (
-                    <BookContainer
-                    id={book.id}
-                    title={book.title}
-                    category={book.category}
-                    key={book.id}
-                    removeBook={removeBook}
-                    />
-                ))}
-            </tbody>
-        </table>
-    </div>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Book ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {books.map((book) => (
+          <BookContainer
+            id={book.id}
+            title={book.title}
+            category={book.category}
+            key={book.id}
+            removeBook={removeBook}
+          />
+        ))}
+      </tbody>
+    </table>
+  </div>
 );
 
 BookList.propTypes = {
-    books: PropTypes.arrayOf(PropTypes.instanceOf(Object)),
-    removeBook: PropTypes.func,
+  books: PropTypes.arrayOf(PropTypes.instanceOf(Object)),
+  removeBook: PropTypes.func,
 };
 
 BookList.defaultProps = {
-    books: [],
-    removeBook: null,
+  books: [],
+  removeBook: null,
 };
 
 export default BookList;
