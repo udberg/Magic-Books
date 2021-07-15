08a593883c9e68451e3cabbfe3c707f78be54f08
udberg <johnuduak066@gmail.com>
2021-07-15 08:20:50 +0100

    Pass handleRemoveBook to book component

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,24 +2,24 @@ const CREATE_BOOK = 'CREATE_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
 
 const defaultBookData = {
-    title: '',
-    category: '',
+  title: '',
+  category: '',
 };
 
 export const createBook = (bookData) => {
-    const id = Math.ceil(Date.now() * Math.random()).toString();
-    const book = {
-        id,
-        ...defaultBookData,
-        ...bookData,
-    };
-    return {
-        type: CREATE_BOOK,
-        payload: { book },
-    };
+  const id = Math.ceil(Date.now() * Math.random()).toString();
+  const book = {
+    id,
+    ...defaultBookData,
+    ...bookData,
+  };
+  return {
+    type: CREATE_BOOK,
+    payload: { book },
+  };
 };
 
 export const removeBook = (id) => ({
-    type: REMOVE_BOOK,
-    payload: { id },
+  type: REMOVE_BOOK,
+  payload: { id },
 });
