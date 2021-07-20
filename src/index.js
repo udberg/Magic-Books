49c3adf3e61dcf30d7c98af34e6fb89d6970bbf2
udberg <johnuduak066@gmail.com>
2021-07-20 22:10:00 +0100

    Create default books

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ const store = createStore(rootReducer);
 /* eslint-disable */
 
 store.dispatch(
-  createBook({ title: 'Between the world and me', category: categories[1] })
+  createBook({ title: 'Between two doors', category: categories[1] })
 );
 store.dispatch(
-  createBook({ title: 'A Gentleman in Moscow', category: categories[5] })
+  createBook({ title: 'Life spent in a dream', category: categories[5] })
 );
 store.dispatch(
-  createBook({ title: 'Atomic Habits', category: categories[2] })
+  createBook({ title: 'Spring Passed By', category: categories[2] })
 );
 
 const MainApp = () => (
