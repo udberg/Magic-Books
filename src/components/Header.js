d54724051e7388c4b4be7aadc33f21f6bad883f3
udberg <johnuduak066@gmail.com>
2021-07-20 10:16:23 +0100

    Create book filter and map dispatch to state and props

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,18 @@ import { NavLink } from 'react-router-dom';
 import image from '../assets/user.svg';
 
 const Header = () => (
-    <header className="header__box">
-        <div className="header__box_innerBox">
-            <h1>Bookstore CMS</h1>
-            <NavLink to="/">Books</NavLink>
-            <NavLink to="/categories">Categories</NavLink>
-        </div>
-        <div>
-            <span className="user__oval">
-                <img src={image} alt="user" />
-            </span>
-        </div>
-    </header>
+  <header className="header__box">
+    <div className="header__box_innerBox">
+      <h1>Bookstore CMS</h1>
+      <NavLink to="/">Books</NavLink>
+      <NavLink to="/categories">Categories</NavLink>
+    </div>
+    <div>
+      <span className="user__oval">
+        <img src={image} alt="user" />
+      </span>
+    </div>
+  </header>
 );
 
 export default Header;
