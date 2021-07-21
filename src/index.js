1bc637340f2bbe2ddaa1e7ff84708d8cb18eef9d
udberg <johnuduak066@gmail.com>
2021-07-21 08:41:32 +0100

    Dispatch book titles and categories

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ const store = createStore(rootReducer);
 /* eslint-disable */
 
 store.dispatch(
-  createBook({ title: 'Between two doors', category: categories[1] })
+  createBook({ title: 'A Gentleman in Moscow', category: categories[1] })
 );
 store.dispatch(
-  createBook({ title: 'Life spent in a dream', category: categories[5] })
+  createBook({ title: 'Between the world and me', category: categories[5] })
 );
 store.dispatch(
-  createBook({ title: 'Spring Passed By', category: categories[2] })
+  createBook({ title: 'Thinking, Fast and Slow', category: categories[2] })
 );
 
 const MainApp = () => (
