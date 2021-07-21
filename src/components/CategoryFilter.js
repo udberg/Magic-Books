90f4779887910f4ae7b2b096f2e4ea52464481e8
udberg <johnuduak066@gmail.com>
2021-07-21 08:33:22 +0100

    Update category filter

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -3,32 +3,32 @@ import PropTypes from 'prop-types';
 import category from '../helper/category';
 
 const CategoryFilter = ({ filter, handleFilterChange }) => {
-    const categories = ['All', ...category];
-    return (
-        <div className="filter__wrapper">
-            <select
-                name="category"
-                value={filter}
-                onChange={(e) => handleFilterChange(e.target.value)}
-                >
-                    {categories.map((item) => (
-                        <option value={item} key={item}>
-                            {item}
-                        </option>
-                    ))}
-            </select>
-        </div>
-    );
+  const categories = ['All', ...category];
+  return (
+    <div className="filter__wrapper">
+      <select
+        name="category"
+        value={filter}
+        onChange={(e) => handleFilterChange(e.target.value)}
+      >
+        {categories.map((item) => (
+          <option value={item} key={item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 };
 
 CategoryFilter.propTypes = {
-    filter: PropTypes.string,
-    handleFilterChange: PropTypes.func,
+  filter: PropTypes.string,
+  handleFilterChange: PropTypes.func,
 };
 
 CategoryFilter.defaultProps = {
-    filter: 'All',
-    handleFilterChange: null,
+  filter: 'All',
+  handleFilterChange: null,
 };
 
 export default CategoryFilter;
