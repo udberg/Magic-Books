f071fdbbb9b45234a1a45f532713c1a9f4ecc986
udberg <johnuduak066@gmail.com>
2021-07-21 08:36:46 +0100

    Create book category

--- src/helper/category.js
+++ src/helper/category.js
@@ -1,11 +1,11 @@
 const categories = [
-    'Action',
-    'Biography',
-    'History',
-    'Horror',
-    'Kids',
-    'Learning',
-    'Sci-Fi',
+  'Action',
+  'Biography',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
 ];
 
 export default categories;
